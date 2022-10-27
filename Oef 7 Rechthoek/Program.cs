da7339a50e07373e1966ece100c374e5f4cc7b49
Kacper Tabaka <kacper.tabaka@student.hogent.be>
2022-10-27 20:31:10 +0200

    r

---
+++ Oef 7 Rechthoek/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Oef_7_Rechthoek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hoogte?: ");
+            int hoogte = int.Parse(Console.ReadLine());
+            int hoogteTeller = 0;
+
+            Console.Write("Breedte?: ");
+            int breedte = int.Parse(Console.ReadLine());
+            
+            do
+            {
+                int breedteTeller = 0; //reset van getal
+                do
+                {
+                    Console.Write("*");
+                    breedteTeller++;
+                }while (breedteTeller < breedte );  
+                
+                Console.WriteLine(); // Naar nieuwe lijngaat
+                hoogteTeller++;
+
+
+            } while (hoogteTeller < hoogte);
+           
+
+
+            
+               
+
+            
+        }
+    }
+}
