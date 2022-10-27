52cd9df690066f5046cc2c74ffd84ac106c80ddd
Kacper Tabaka <kacper.tabaka@student.hogent.be>
2022-10-27 19:35:06 +0200

    fdg

---
+++ Oef 5 Grootste getal/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Oef_5_Grootste_getal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal = 0;
+            int grootsteGetal = 0;
+            int teller = 0;
+
+            do
+            {
+                Console.WriteLine("Geef getal in :");
+                getal = int.Parse(Console.ReadLine());
+
+                if (getal > grootsteGetal)
+                {
+                    grootsteGetal = getal;
+
+                }
+            
+            } while (getal != -1);
+
+            Console.WriteLine($"Grootste getal is {grootsteGetal}");
+            
+
+
+        }
+    }
+}
---
+++ Oef 6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Oef_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int teller = 0;
+            int getal = 0;
+            int grootsteGetal = 0;
+            
+            do
+            {
+                Console.WriteLine("Geef getal in : (stop met -1)");
+                getal = int.Parse(Console.ReadLine());
+
+                if(getal > grootsteGetal)
+                {
+                    grootsteGetal = getal;
+                  // als het grotere getal was ingevoert onze teller reset tot 1   
+                    teller = 1;
+                }else if (getal == grootsteGetal)
+                {
+                    teller++;
+                }
+            }while(getal != -1);
+
+            Console.WriteLine($"Grootste getal is {grootsteGetal} en heeft {teller} keer voorgekomen");
+        }
+    }
+}
