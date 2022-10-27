557033a954ffc96b6bcec763dd18c5922056e6c7
Kacper Tabaka <kacper.tabaka@student.hogent.be>
2022-10-27 15:46:07 +0200

    OEF 3

---
+++ Oef 2 tafel van 7/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Oef_2_tafel_van_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int teller = 1 ;
+            int maal = 7;
+
+
+            do
+            {
+                int antwoord = teller * maal;
+                Console.WriteLine($"{teller} x {maal} = {antwoord}");
+                teller++ ;
+            } while (teller <= 10);
+        }
+    }
+}
---
+++ Oef 3 som/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Oef_3_som
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int som = 0;
+            int getal;
+            do
+            {
+                Console.WriteLine("geef getal in :");
+                getal = int.Parse(Console.ReadLine());
+
+                som += getal;
+
+            } while (getal != -1);
+
+            som -= getal;
+            
+            Console.WriteLine($"De som is {som }");
+        }
+    }
+}
