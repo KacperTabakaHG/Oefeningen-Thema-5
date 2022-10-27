b6b8f09bc326c3c9261da7e7f6ae801f0dda73ed
Kacper Tabaka <kacper.tabaka@student.hogent.be>
2022-10-27 20:39:11 +0200

    f

---
+++ Oefening 8 Vierkant/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Oefening_8_Vierkant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zijde?: ");
+            int zijde = int.Parse(Console.ReadLine());
+            int hoogteTeller = 0;
+
+            do
+            {
+                int zijdeTeller = 0;
+                do
+                {
+                    Console.Write("*");
+                    zijdeTeller++;
+                } while (zijdeTeller < zijde);
+                Console.WriteLine();
+                hoogteTeller++;
+            } while (hoogteTeller < zijde);
+        }
+    }
+}
