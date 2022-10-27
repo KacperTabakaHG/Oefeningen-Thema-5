232517441a52a7a55e4ab683f88757b225708fb5
Kacper Tabaka <kacper.tabaka@student.hogent.be>
2022-10-27 21:00:03 +0200

    f

---
+++ Oef 9 Double/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Oef_9_Double
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double getal;
+            do
+            {
+                Console.WriteLine("Geef een double getal in");
+                if (double.TryParse(Console.ReadLine(), out getal))
+                {
+                    Console.WriteLine("Dank je voor het dubbel getal");
+                }
+                else
+                {
+                    Console.WriteLine("Eide (Wegens geen double getal)");
+                    break;
+                }
+                
+
+            } while(true);
+            
+        }
+    }
+}
