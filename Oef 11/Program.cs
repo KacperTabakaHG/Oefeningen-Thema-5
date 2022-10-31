9dcef927f4f7cdfd275498ac788cde6d2a5f5be0
Kacper Tabaka <kacper.tabaka@student.hogent.be>
2022-10-31 22:08:34 +0100

    d

---
+++ Oef 11/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Oef_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int getalEen = int.Parse(Console.ReadLine());
+            string opeRator = Console.ReadLine();
+            
+            
+            while (opeRator != "=")
+            {
+                int getal = int.Parse(Console.ReadLine());
+                
+                if (opeRator == "+")
+                {
+                    getalEen = getalEen + getal;
+                } else if (opeRator == "-")
+                {
+                    getalEen = getalEen - getal;
+                }
+                
+                opeRator = Console.ReadLine();
+            }
+            Console.WriteLine(getalEen);
+        }
+    }
+}
