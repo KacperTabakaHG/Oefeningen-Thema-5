f93fedbb75f27db72e946bfb47c1b570b13e574d
Kacper Tabaka <kacper.tabaka@student.hogent.be>
2022-11-03 17:40:38 +0100

    f

---
+++ Oef 16/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Oef_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal;
+            int som = 0;
+            string getalAlsTekst;
+            bool geenStopIngetypt; // (1)
+            do
+            {
+                getalAlsTekst = Console.ReadLine();
+
+                geenStopIngetypt = (getalAlsTekst.Trim().ToUpper() != "STOP"); // (1)
+
+                bool invoerOk = int.TryParse(getalAlsTekst, out getal);
+                if (invoerOk)
+                {
+                    // gebruiker een getal heeft ingetypt
+                    som += getal;
+                    Console.WriteLine("+");
+                }
+                else if (geenStopIngetypt) // (1)
+                {
+                    // andere tekst ingetypt
+                    Console.WriteLine("Gelieve een geheel getal in te voeren (of STOP om te stoppen).");
+
+                }
+            } while (geenStopIngetypt); // (1)
+
+            Console.WriteLine("=");
+            Console.WriteLine(som);
+
+
+
+
+
+
+        }
+    }
+}
